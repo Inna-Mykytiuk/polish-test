c9e3b3fd2939307fe49cddd5f437c8bbc9ca75a0
Inna-Mykytiuk <innka-pinnka@ukr.net>
2024-07-16 20:47:00 +0300

    create slider

--- index.js
+++ index.js
@@ -12,3 +12,23 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const name = document.getElementById('name').value;
+  const phone = document.getElementById('phone').value;
+  const email = document.getElementById('email').value;
+  const consent = document.getElementById('consent').checked;
+
+  if (consent) {
+    console.log('Form Submitted');
+    console.log('Name:', name);
+    console.log('Phone:', phone);
+    console.log('Email:', email);
+
+    alert('Form Submitted Successfully!');
+  } else {
+    alert('You must agree to the terms to submit the form.');
+  }
+});
+
