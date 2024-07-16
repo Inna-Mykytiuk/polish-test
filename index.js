3d4e22c3275db980d98e25519b8a1b81ac4d8e05
Inna-Mykytiuk <innka-pinnka@ukr.net>
2024-07-16 19:19:30 +0300

    service section

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function () {
+  const burgerBtn = document.querySelector('.burger-btn');
+  const closeBtn = document.querySelector('.close-btn');
+  const mobileMenu = document.querySelector('.mobile-menu');
 
+  burgerBtn.addEventListener('click', function () {
+    mobileMenu.classList.add('active');
+  });
+
+  closeBtn.addEventListener('click', function () {
+    mobileMenu.classList.remove('active');
+  });
 });
 
